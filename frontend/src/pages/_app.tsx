eeb4e6a081547a949ef5171d491a18e093438b13
Cointastig <k.pfaender1989@gmail.com>
2025-07-13 00:30:07 +0200

    Update _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import '@/styles/globals.css';
+import '../styles/globals.css';   // ← alias entfernt
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
